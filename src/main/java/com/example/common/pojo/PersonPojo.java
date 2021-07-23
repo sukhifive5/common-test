b8b7fb6d300101e5753d60b168e9f1b5fc4b926a
Barjinder Singh <sukhifive@gmail.com>
2021-07-23 13:37:07 -0700

    Initial commit

---
+++ src/main/java/com/example/common/pojo/PersonPojo.java
@@ -0,0 +1,31 @@
+package com.example.common.pojo;
+
+public class PersonPojo {
+
+    String firstName;
+    String lastName;
+
+    @Override
+    public String toString() {
+        return "PersonPojo{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
